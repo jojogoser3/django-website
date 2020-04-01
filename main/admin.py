6eb4cb20fa9725bf206ca31535f8454eb43bb21c
jojogoser3 <55130268+jojogoser3@users.noreply.github.com>
2020-04-01 13:06:56 +0200

    Homepage created with styling, admin edit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.contrib import admin
+from .models import Tutorial
+from tinymce.widgets import TinyMCE
+from django.db import models
 
-# Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("Title/date", {'fields': ["tutorial_title", "tutorial_published"]}),
+        ("Content", {"fields": ["tutorial_content"]})
+    ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+        }
+
+
+admin.site.register(Tutorial,TutorialAdmin)
